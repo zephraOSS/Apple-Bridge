b5d9554a175ddc7e1f27e25d4a6b0f313d8f85c6
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-02-16 16:06:17 +0100

    jsDoc comments

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ class AppleBridge {
         };
     }
 
+    /**
+     * Listen to events
+     * @param {String} event
+     * @param {String} service
+     * @param {Function} callback
+     */
     on(event, service, callback) {
         if (!this._events[service]) return;
         if (!this._events[service][event]) this._events[service][event] = [];
@@ -13,6 +19,12 @@ class AppleBridge {
         this._events[service][event].push(callback);
     }
 
+    /**
+     * Emit an event
+     * @param {String} event
+     * @param {String} service
+     * @param  {...any} args
+     */
     emit(event, service, ...args) {
         if (!this._events[service] || !this._events[service][event]) return;
 
@@ -21,6 +33,12 @@ class AppleBridge {
         });
     }
 
+    /**
+     * Remove listener
+     * @param {String} event
+     * @param {String} service
+     * @param {Function} callback
+     */
     off(event, service, callback) {
         if (!this._events[service] || !this._events[service][event]) return;
 
@@ -29,6 +47,12 @@ class AppleBridge {
         );
     }
 
+    /**
+     * Listen to events (once)
+     * @param {String} event
+     * @param {String} service
+     * @param {Function} callback
+     */
     once(event, service, callback) {
         const self = this;
 
@@ -40,24 +64,52 @@ class AppleBridge {
         this.on(event, service, onceCallback);
     }
 
+    /**
+     * Get instance
+     * @returns {AppleBridge}
+     */
     static getInstance() {
         if (!this._instance) this._instance = new AppleBridge();
 
         return this._instance;
     }
 
+    /**
+     * Listen to events
+     * @param {String} event
+     * @param {String} service
+     * @param {Function} callback
+     */
     static on(event, service, callback) {
         this.getInstance().on(event, service, callback);
     }
 
+    /**
+     * Emit an event
+     * @param {String} event
+     * @param {String} service
+     * @param  {...any} args
+     */
     static emit(event, service, ...args) {
         this.getInstance().emit(event, service, ...args);
     }
 
+    /**
+     * Remove listener
+     * @param {String} event
+     * @param {String} service
+     * @param {Function} callback
+     */
     static off(event, service, callback) {
         this.getInstance().off(event, service, callback);
     }
 
+    /**
+     * Listen to events (once)
+     * @param {String} event
+     * @param {String} service
+     * @param {Function} callback
+     */
     static once(event, service, callback) {
         this.getInstance().once(event, service, callback);
     }
