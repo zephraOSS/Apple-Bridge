385b175c3eda5fbe90cac16afa64e43c660d32e8
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-10-14 13:27:53 +0200

    [AMR-9] feat: update test

--- test.js
+++ test.js
@@ -5,10 +5,9 @@ log("Starting...");
 
 bridge.on("playing", "music", (data) => {
     log("playing", "music", data);
+    process.exit();
 });
 
 function log(...args) {
     console.log("[TEST]", ...args);
 }
-
-process.exit();
